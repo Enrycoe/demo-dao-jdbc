9ba777060957ade795af0d3561d4a701519f4f56
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-04-25 09:25:29 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 			
+		LocalDate d01 = LocalDate.now();
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", d01, 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 
 }
